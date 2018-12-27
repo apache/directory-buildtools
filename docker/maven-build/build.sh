349b1b482cf5968feac78b53bf7b1966409dc499
Stefan Seelmann <mail@stefan-seelmann.de>
2018-12-27 10:09:40 +0100

    Limit memory usage

--- docker/maven-build/build.sh
+++ docker/maven-build/build.sh
@@ -6,10 +6,11 @@ set -e
 # trace commands
 set -x
 
-# Temporary workaround with Surefire on Debian with Java 8
+# Temporary workaround with Surefire on Debian with Java 8 and limit memory usage
 # https://issues.apache.org/jira/browse/SUREFIRE-1588
 # https://stackoverflow.com/questions/53010200/maven-surefire-could-not-find-forkedbooter-class
-export _JAVA_OPTIONS=-Djdk.net.URLClassPath.disableClassPathURLCheck=true
+export MAVEN_OPTS="-Xmx1024m"
+export _JAVA_OPTIONS="-Djdk.net.URLClassPath.disableClassPathURLCheck=true -Xmx1024m"
 
 mvn -V clean install
 
